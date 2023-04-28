6338f79ac411ace4ef03b0daca027a6fdcfa2177
tegar <eategar@gmail.com>
2023-04-29 00:00:40 +0700

    fix typo

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class FoodController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource
      */
 
     // api
